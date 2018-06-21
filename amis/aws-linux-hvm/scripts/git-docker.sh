1b80b6699cd4df4b3f1e42b531a7e62562ae4ee1
Brant Burnett <bburnett@centeredgesoftware.com>
2018-06-21 11:07:25 -0400

    Complete Jenkins linux build agent script (PHNX-1048)
    Motivation
    ----------
    Base AMI for a Linux build agent with Docker and other required
    compenents preinstalled.

--- amis/aws-linux-hvm/scripts/git-docker.sh
+++ amis/aws-linux-hvm/scripts/git-docker.sh
@@ -1,20 +1,16 @@
-#!/bin/sh
-
-sleep 20
+#!/bin/bash
 
 # Remove old Java
 sudo yum remove -y java-1.7.0-openjdk
 
 # Install Java and Git
 # This script assumes Docker is baked into the AMI
-sudo yum install -y git java-1.8.0
+sudo yum install -y git git-lfs java-1.8.0
 
-# Configure Docker for ephemeral storage
-sudo mkdir -p /media/ephemeral0/docker
-echo "OPTIONS=\"\$OPTIONS -g /media/ephemeral0/docker\"" | sudo tee -a /etc/sysconfig/docker
-sudo chkconfig docker on
+# Ensure Docker access for ec2-user
+sudo gpasswd -a ec2-user docker
 
 # Install docker-compose
-dockerComposeVersion=1.16.1
+dockerComposeVersion=1.21.2
 sudo -i curl -L https://github.com/docker/compose/releases/download/$dockerComposeVersion/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
