7d72c85e4ecc65e0acfd9882c36c438a8588d8a5
Brant Burnett <bburnett@centeredgesoftware.com>
2018-10-12 11:34:04 -0700

    Update Windows agent for Stage (STAGE-3284) (#4)
    Motivation
    ----------
    Windows agent for Jenkins isn't working effectively.
    
    Modifications
    -------------
    Disable CredSSP auth for WinRM, as this is breaking Jenkins ability to
    connect to the agent.
    
    Add detection of failed Chocolatey package installs.
    
    Perform ngen after installations to improve startup performance.
    
    Include git-lfs for large file handling in Git.
    
    Disable the Git for Windows credential helper to fix Git speed issues
    when running on an agent with no UI.
    
    https://centeredge.atlassian.net/browse/STAGE-3284

--- amis/windows/scripts/windows10sdk.ps1
+++ amis/windows/scripts/windows10sdk.ps1
@@ -3,3 +3,8 @@ $ErrorActionPreference = 'Stop'
 Invoke-WebRequest -UseBasicParsing http://download.microsoft.com/download/2/1/2/2122BA8F-7EA6-4784-9195-A8CFB7E7388E/StandaloneSDK/sdksetup.exe -OutFile sdksetup.exe
 
 Start-Process -FilePath "sdksetup.exe" -ArgumentList /Quiet, /NoRestart, /Log, c:\install_logs\sdksetup.log -NoNewWindow -Wait
+
+& ${env:windir}\microsoft.net\framework\v4.0.30319\ngen.exe update /force /queue
+& ${env:windir}\microsoft.net\framework64\v4.0.30319\ngen.exe update /force /queue
+& ${env:windir}\microsoft.net\framework\v4.0.30319\ngen.exe executequeueditems
+& ${env:windir}\microsoft.net\framework64\v4.0.30319\ngen.exe executequeueditems
