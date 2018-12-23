ae88fdce5e0250c5ae8f9cd5e6b2c9d9e1d660f5
Brant Burnett <bburnett@centeredgesoftware.com>
2018-12-23 09:32:19 -0500

    Various improvements during move to GenDev env
    Modifications
    -------------
    Upgraded to Amazon Linux 2 as the base image for the Linux build agent.
    
    Changed the Linux build agent to automatically pick the latest AMI from
    the correct category.
    
    Upgraded to docker-compose 1.23.1 on the Linux build agent.
    
    Build Linux image with ENA enabled so we can use m5 instances.
    
    Create encrypted AMIs so that our build agents run on an encrypted
    volume for added security.

--- amis/aws-linux-hvm/scripts/git-docker.sh
+++ amis/aws-linux-hvm/scripts/git-docker.sh
@@ -5,12 +5,16 @@ sudo yum remove -y java-1.7.0-openjdk
 
 # Install Java and Git
 # This script assumes Docker is baked into the AMI
+sudo yum update -y
 sudo yum install -y git git-lfs java-1.8.0
 
+# Install Docker
+sudo amazon-linux-extras install docker
+
 # Ensure Docker access for ec2-user
-sudo gpasswd -a ec2-user docker
+sudo usermod -a -G docker ec2-user
 
 # Install docker-compose
-dockerComposeVersion=1.21.2
+dockerComposeVersion=1.23.2
 sudo -i curl -L https://github.com/docker/compose/releases/download/$dockerComposeVersion/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
