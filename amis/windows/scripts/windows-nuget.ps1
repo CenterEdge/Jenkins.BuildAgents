05bb6b9d3da22fe23bbc984d41038d7547ea93f0
Brant Burnett <bburnett@centeredgesoftware.com>
2020-11-11 10:04:24 -0500

    Add .NET 5.0 to Windows agents (DO-987)
    Motivation
    ----------
    C# 9 goodness.
    
    Modifications
    -------------
    Adjust TLS settings to be modern, and add .NET 5.0 to the AMI build
    scripts.

--- amis/windows/scripts/windows-nuget.ps1
+++ amis/windows/scripts/windows-nuget.ps1
@@ -2,6 +2,8 @@ $ErrorActionPreference = 'Stop'
 
 New-Item -Type Directory $Env:ProgramFiles\NuGet | Out-Null
 
+[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
+
 Invoke-WebRequest -UseBasicParsing https://dist.nuget.org/win-x86-commandline/v$Env:NUGET_VERSION/nuget.exe -OutFile $Env:ProgramFiles\NuGet\nuget.exe
 
 [Environment]::SetEnvironmentVariable("PATH", ${Env:PATH} + ";${Env:ProgramFiles}\NuGet\", "Machine")
