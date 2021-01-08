e207243d0957541f9e2c9346682fe3bff5cfda62
djonesCenterEdge <61988618+djonesCenterEdge@users.noreply.github.com>
2021-01-08 08:37:19 -0500

    Create script for github CLI(ADV-17291) (#12)
    Motivation
    ------------
    We need a script to install the guthub cli and add the path to the system variables, the gh cli installer only adds it to the user running it, with no way to specify other/all users/system. I also ran into an issue where multiple semi-colons can be chained together, this break setx, it will throw an error saying the default option cannot be specifed more than once. I added checks for semi colons in hopes it would prevent future issues.
    
    Modification
    -------------
     - Added script for git hub cli
     - Added checks for semicolons at the end of path
    
    https://centeredge.atlassian.net/browse/ADV-17291

--- amis/windows/scripts/windows-nuget.ps1
+++ amis/windows/scripts/windows-nuget.ps1
@@ -6,4 +6,11 @@ New-Item -Type Directory $Env:ProgramFiles\NuGet | Out-Null
 
 Invoke-WebRequest -UseBasicParsing https://dist.nuget.org/win-x86-commandline/v$Env:NUGET_VERSION/nuget.exe -OutFile $Env:ProgramFiles\NuGet\nuget.exe
 
-[Environment]::SetEnvironmentVariable("PATH", ${Env:PATH} + ";${Env:ProgramFiles}\NuGet\", "Machine")
+if (${Env:PATH}.EndsWith(';')) {
+    $path = "${Env:PATH}${Env:ProgramFiles}\NuGet\"
+}
+else {
+    $path = "${Env:PATH};${Env:ProgramFiles}\NuGet\"
+}
+
+[Environment]::SetEnvironmentVariable("PATH", $path, "Machine")
