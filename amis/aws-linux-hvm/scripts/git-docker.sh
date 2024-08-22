67cb9202a325999095a32a91fd06593aefd5bd4d
Ara Yapejian <ayapejian@centeredgesoftware.com>
2024-08-22 01:43:06 -0400

    Upgrade JVM for Jenkins Agents (DO-4077)

--- amis/aws-linux-hvm/scripts/git-docker.sh
+++ amis/aws-linux-hvm/scripts/git-docker.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
+sudo dnf install -y git git-lfs java-17-amazon-corretto docker
+sudo usermod -aG docker ec2-user
+sudo systemctl enable --now docker.service containerd.service
 
-sudo yum install -y git git-lfs
-sudo amazon-linux-extras install java-openjdk11 docker
-# Autostart / Ensure Docker access for ec2-user
-sudo systemctl enable docker
-sudo usermod -a -G docker ec2-user
-
-# Install docker-compose
-dockerComposeVersion=1.23.2
-sudo -i curl -L https://github.com/docker/compose/releases/download/$dockerComposeVersion/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+dockerComposeVersion=v2.20.2
+sudo curl -sL https://github.com/docker/compose/releases/download/$dockerComposeVersion/docker-compose-linux-"$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+
