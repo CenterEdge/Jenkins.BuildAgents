dc667e0725002769429796254a29e874a4285817
Ara Yapejian <ayapejian@centeredgesoftware.com>
2024-08-26 01:04:41 -0400

    Revert docker-compose to 1.x version due to build issues

--- amis/aws-linux-hvm/scripts/git-docker.sh
+++ amis/aws-linux-hvm/scripts/git-docker.sh
@@ -3,7 +3,7 @@ sudo dnf install -y git git-lfs java-17-amazon-corretto docker
 sudo usermod -aG docker ec2-user
 sudo systemctl enable --now docker.service containerd.service
 
-dockerComposeVersion=v2.20.2
-sudo curl -sL https://github.com/docker/compose/releases/download/$dockerComposeVersion/docker-compose-linux-"$(uname -m)" -o /usr/local/bin/docker-compose
+# Install docker-compose
+dockerComposeVersion=1.23.2
+sudo -i curl -L https://github.com/docker/compose/releases/download/$dockerComposeVersion/docker-compose-linux-"$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-
