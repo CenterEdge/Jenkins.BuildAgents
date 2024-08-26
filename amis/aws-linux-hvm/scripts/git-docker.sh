4051540358ca33b207585d0ee19036f19d8551b1
Ara Yapejian <47781818+ayapejian-centeredge@users.noreply.github.com>
2024-08-26 08:12:05 -0400

    Upgrade jenkins linux agents JVM (DO-4077) (#25)
    Motivation
    ---
    JVM 11 is EOL soon, upgrade to JVM 17
    
    Modification
    ---
    * Updated version of java
    
    Result
    ---
    Build agent AMI created and new AMI id `ami-0ab69148e939f5888` is being used in jenkins server now
    
    https://centeredge.atlassian.net/browse/DO-4077

--- amis/aws-linux-hvm/scripts/git-docker.sh
+++ amis/aws-linux-hvm/scripts/git-docker.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-
-sudo yum install -y git git-lfs
-sudo amazon-linux-extras install java-openjdk11 docker
-# Autostart / Ensure Docker access for ec2-user
-sudo systemctl enable docker
-sudo usermod -a -G docker ec2-user
+sudo dnf install -y git git-lfs java-17-amazon-corretto docker
+sudo usermod -aG docker ec2-user
+sudo systemctl enable --now docker.service containerd.service
 
 # Install docker-compose
 dockerComposeVersion=1.23.2
-sudo -i curl -L https://github.com/docker/compose/releases/download/$dockerComposeVersion/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo -i curl -L https://github.com/docker/compose/releases/download/$dockerComposeVersion/docker-compose-linux-"$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
